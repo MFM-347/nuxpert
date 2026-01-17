dc18404a698661f1b2e12d10027088857491fec8
Farhan Madni <madnifm347@gmail.com>
2026-01-17 19:04:44 +0500

    Add Google Site Verification to SEO metadata

--- app/app.vue
+++ app/app.vue
@@ -3,6 +3,8 @@ useSeo({
   meta: { author: 'art70x' },
   icons: { favicon: '/favicon.ico', icon: '/icon.svg', appleTouchIcon: '/apple-touch-icon.png' },
 })
+
+useSeoMeta({ googleSiteVerification: "9C3mmoooLN2NLh5PUEVgr98hTKYWPIarhqp7Efdfwuo" })
 </script>
 
 <template>
